f142a2caa3e45d8f41bcb5d3d2e9c64999bfd7c5
anitha <anithareddy41@gmail.com>
2017-08-04 14:48:55 -0700

    modified file

--- javascript-topics/promiseexmple.js
+++ javascript-topics/promiseexmple.js
@@ -9,15 +9,13 @@ for(let i=0; i<5;++i){
     }, 100);
 }
 //compilation Phase:var
-//1. var is declared in the global scope
-//2 var is assigned to i
+//1. i is declared as global scope
 //execution phase:
 //1. i is assigned to zero 
 //2. i is printed for each time after completion of loop 
 
 // Compilation phase:let
-//1. let is declared as function scope
-//2. i is assigned to let
+//1.i is declared block scope
 //execution phase
 //1.initially i is assigned to zero;i=0
 //2. as let is functionscope, i will be increased by one and prints
