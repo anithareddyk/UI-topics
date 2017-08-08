0dc944016d348bfd8075c067b2b81a86c6a46b61
anitha <anithareddy41@gmail.com>
2017-08-08 16:43:06 -0700

    component

---
+++ react-topics/components.js
@@ -0,0 +1,10 @@
+// An element describing a component is also an element, just like an element describing the DOM node. 
+// They can be nested and mixed with each other.
+// Example: a small snippet would be as 
+{
+  type: select,
+  props: {
+    color: 'green',
+    children: 'OK!'
+  }
+}
