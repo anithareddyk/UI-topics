89986b5c42a6388550f2cf1c1f03c28f3fe2d9d1
anitha <anithareddy41@gmail.com>
2017-08-08 17:28:28 -0700

    component

---
+++ react-topics/componentsandProps.js
@@ -0,0 +1,27 @@
+// Components let you split the UI into independent, reusable pieces, 
+// and think about each piece in isolation.
+
+// Conceptually, components are like JavaScript functions. 
+// They accept arbitrary inputs (called "props") and return React elements describing what should appear on the screen.
+const element = <Welcome name="Anitha" />;
+// When React sees an element representing a user-defined component, 
+// it passes JSX attributes to this component as a single object. We call this object "props".
+
+function Comment(props) {
+  return (
+    <div className="UI">
+      <div className="Training">
+        
+        <div className="UserInfo">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="UI-text">
+        {props.text}
+      </div>
+      <div className="UI-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
